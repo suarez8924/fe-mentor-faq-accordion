3df63fc4a3fb56cbfd4a9610304494436d1cab9f
suarez8924 <34613811+suarez8924@users.noreply.github.com>
2024-06-07 21:40:12 +0100

    finish development

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Attribution from './components/attribution/Attribution';
+import FaqCard from './components/faq-card/FaqCard';
+
+function App() {
+  return (
+    <main className="main">
+      <FaqCard />
+      <Attribution />
+    </main>
+  );
+}
+
+export default App;
